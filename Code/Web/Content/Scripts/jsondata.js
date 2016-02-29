5b86403193638d5c331fafe24d00dcf9efeeb5b8
unknown <oscar.marin@CR2080421W1.ey.net>
2016-02-29 11:26:32 -0600

    added data to be consumed by the app

---
+++ Code/Web/Content/Scripts/jsondata.js
@@ -0,0 +1,46 @@
+﻿var mainData = {
+    engines: [
+        {
+            code: "ENGINE-01", name: "V2500 Engine", picture: "http://www.pw.utc.com/Content/V2500_Engine/img/V2500engine.jpg",
+            features: ["feature 1", "feature 2", "feature 3"],
+            variables: [
+                { name: "vibration", target: 50 },
+                { name: "flight hours", target: 500 },
+                { name: "EGTM", target: 200 },
+            ],
+            parts: [
+                { code: "PART-01", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" },
+                { code: "PART-02", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" },
+                { code: "PART-03", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" }
+            ]
+        },
+        {
+            code: "ENGINE-02", name: "GP7200 ENGINE", picture: "http://www.pw.utc.com/Content/GP7200_Engine/img/_GP7200engine.jpg",
+            features: ["feature 1", "feature 2", "feature 3"],
+            variables: [
+                { name: "vibration", target: 50 },
+                { name: "flight hours", target: 500 },
+                { name: "EGTM", target: 200 },
+            ],
+            parts: [
+                { code: "PART-01", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" },
+                { code: "PART-02", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" },
+                { code: "PART-03", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" }
+            ]
+        },
+        {
+            code: "ENGINE-03", name: "PW4000-94 ENGINE", picture: "http://www.pw.utc.com/Content/PW400094_Engine/img/PW4000-94engine.jpg",
+            features: ["feature 1", "feature 2", "feature 3"],
+            variables: [
+                { name: "vibration", target: 50 },
+                { name: "flight hours", target: 500 },
+                { name: "EGTM", target: 200 },
+            ],
+            parts: [
+                { code: "PART-01", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" },
+                { code: "PART-02", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" },
+                { code: "PART-03", name: "Part Name", description: "part description", picture: "http://preview.turbosquid.com/Preview/2014/07/09__07_03_36/Part2226.jpgf5b4600e-6765-44ee-8324-886d34d5162fLarge.jpg" }
+            ]
+        }
+    ]
+};
